4d17d2db702f7a8d7b106e3500622a2a0ec90c16
Дени <deni@MacBookAir.Dlink>
2024-12-03 20:43:04 +0300

    Первая задача с JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,33 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService us = new UserServiceImpl();
+
+        us.createUsersTable();
+
+        us.saveUser("Ivan", "Ivanov", (byte) 10);
+        us.saveUser("Titan", "Titanov", (byte) 40);
+        us.saveUser("Opal", "Opalov", (byte) 20);
+        us.saveUser("Chumak", "Chumakov", (byte) 35);
+        System.out.println("Пользователи успешно созданы");
+
+        System.out.println(us.getAllUsers());
+        System.out.println("Список всех пользователей");
+
+        us.cleanUsersTable();
+
+        us.dropUsersTable();
+
+
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,9 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.Session;
+import org.hibernate.SessionFactory;
+
+import java.lang.module.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,8 +12,17 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/userdb";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Alaev001";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
+
+//    public static SessionFactory getSessionFactory() {
+//        if (sessionFactory == null) {
+//            try {
+//                Configuration configuration = new Configuration();
+//            }
+//        }
+//    }
 }
